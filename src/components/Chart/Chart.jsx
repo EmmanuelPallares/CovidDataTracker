a949a197c00e0b289f7046b23b6fde713db4429b
emmanuel <emmanuelpallares50@gmail.com>
2021-05-31 23:40:00 -0500

    first version finished

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -25,10 +25,11 @@ const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
           labels: ['Infected', 'Recovered', 'Deaths'],
           datasets: [
             {
-              label: 'People',
+              label: 'People ' ,
               backgroundColor: ['rgba(0, 0, 255, 0.5)', 'rgba(0, 255, 0, 0.5)', 'rgba(255, 0, 0, 0.5)'],
               data: [confirmed.value, recovered.value, deaths.value],
             },
+            
           ],
         }}
         options={{
